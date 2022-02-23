b50ba724a4db368112bbc03850652e4c92e77ee5
Olivia isberg <oliviaisberg@gmail.com>
2022-02-23 10:25:35 +0100

    Created footer component and added styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/Footer';
 import StartPage from './pages/Startpage';
 
 function App() {
   return (
-    <StartPage/ >
+    <StartPage/>
+
   );
 }
 
