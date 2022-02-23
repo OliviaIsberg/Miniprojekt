96b97b0a456ddcf638537641aef7340aa712b0f3
Olivia isberg <oliviaisberg@gmail.com>
2022-02-23 09:52:15 +0100

    Created header and added css styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
+import './Header.css';
 function Header() {
     return(
-        <div>
-            Happy hacking
+        <div className="headerContainer">
+            <header>
+                <h1 className='headerHeading'>Header</h1>
+            </header>
         </div>
     )
 }
