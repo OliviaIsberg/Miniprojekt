c8e079d9aa9788817175c402cc5c19836cba3c55
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-02-24 10:02:16 +0100

    add interface.tsx

--- src/components/CardsContainer.tsx
+++ src/components/CardsContainer.tsx
@@ -1,26 +1,20 @@
-import { CSSProperties, useEffect, useState } from 'react'
-import SmallDrinkCard from './SmallDrinkCard/SmallDrinkCard'
-
-interface Props {}
-
-interface drinks {
-  strDrink: string
-  strDrinkThumb: string
-  idDrink: string
-}
+import { CSSProperties, useEffect, useState } from "react";
+import SmallDrinkCard from "./SmallDrinkCard/SmallDrinkCard";
+import { Drink } from "../Interfaces";
+import { Props } from "../Interfaces";
 
 const CardsContainer = (props: Props) => {
-  const [drinks, setDrinks] = useState([] as drinks[])
+  const [drinks, setDrinks] = useState([] as Drink[]);
 
   useEffect(() => {
     fetch(`https://thecocktaildb.com/api/json/v1/1/filter.php?i=gin`)
       .then((res) => {
-        return res.json()
+        return res.json();
       })
       .then((data) => {
-        return setDrinks(data.drinks)
-      })
-  }, [])
+        return setDrinks(data.drinks);
+      });
+  }, []);
 
   return (
     <div style={rootStyle}>
@@ -33,16 +27,16 @@ const CardsContainer = (props: Props) => {
           />
         ))}
     </div>
-  )
-}
+  );
+};
 
-export default CardsContainer
+export default CardsContainer;
 
 const rootStyle: CSSProperties = {
-  display: 'flex',
-  flexWrap: 'wrap',
-  gap: '2rem',
-  alignItems: 'center',
-  justifyContent: 'center',
-  padding: '2rem',
-}
+  display: "flex",
+  flexWrap: "wrap",
+  gap: "2rem",
+  alignItems: "center",
+  justifyContent: "center",
+  padding: "2rem",
+};
