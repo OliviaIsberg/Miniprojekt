7dd691cecdbd3c8dad0c31675b628e537a305960
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-02-24 10:02:00 +0100

    add interfaces Fixes #18

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Footer from './Footer';
+import Layout from './Layout';
+import StartPage from '../pages/Startpage';
+
+function App() {
+  return (
+    <Layout/>
+
+  );
+
+}
+
+export default App
