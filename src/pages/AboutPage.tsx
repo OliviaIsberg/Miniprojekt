0b9ac04225212bc7fcced1bce1d1b4387940dea1
Sabina Andersson <sabina.andersson@medieinstitutet.se>
2022-02-25 14:52:47 +0100

    added taxt + avatars to about page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,12 +1,52 @@
+import "./AboutPage.css";
+import tomas from "./tomas.png";
+import olivia from "./olivia.png";
+import sabina from "./sabina.png";
+
 function AboutPage() {
   return (
     <>
       <div className="textContainer">
-        <h2>About this page </h2>
-        <p> lorem ipsum jdskaljdksajdklsajdlsakdskladjdjskjdksdjasldjkdkasl </p>
+        <div>
+          <h2>About Let's Drink </h2>
+          <p>
+            This application is built by three front-end webdeveloper students
+            at Medieinstitutet Sweden. The goal was to build the application in
+            TSX (React Typescript) where all data was fetched from an API,
+            explore more about how data is drawn out, and use React routing.
+          </p>
+          <h3>Features:</h3>
+          <ul>
+            <li>
+              Filter drinks by type of booze (either by clicking the cards on
+              the first page, or by going to
+              http://localhost:3000//alcohol/examplealcohol)
+            </li>
+            <li>
+              Search for drinks by the name. A search bar on the first page that
+              automatically goes through the full API by the drink-names.
+            </li>
+            <li>
+              Random drink - button. A button places in the header that fetches
+              one randomized drink.
+            </li>
+          </ul>
+          <p>
+            The API used is by "thecocktaildb" :{" "}
+            <a href="https://thecocktaildb.com/">https://thecocktaildb.com/</a>
+          </p>
+
+          <h3>Coders:</h3>
+          
+          <ul className="devList">
+            <li><img src={tomas}></img>Tomas Fridekrans</li>
+            <li><img src={olivia}></img>Olivia Isberg</li>
+            <li><img src={sabina}></img>Sabina Andersson</li>
+          </ul>
+          </div>
       </div>
     </>
-  )
+  );
 }
 
-export default AboutPage
+export default AboutPage;
