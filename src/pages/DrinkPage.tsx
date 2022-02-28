699cc369a66447a0cfd942e9f25905945342c0e3
Olivia isberg <oliviaisberg@gmail.com>
2022-02-28 13:25:24 +0100

    show ingredients and measurements on drinks in  drinkpage

--- src/pages/DrinkPage.tsx
+++ src/pages/DrinkPage.tsx
@@ -3,13 +3,44 @@ import {useParams} from 'react-router-dom';
 import "./DrinkPage.css";
 import { Drink } from "../Interfaces";
 
-interface Props {
-  drinks: Drink[];
+function getIngredients(drink: Drink | undefined) {
+  if(drink === undefined) {
+    return null;
+  }
+
+  const info = [
+    {'ingredient': drink.strIngredient1, 'measure': drink.strMeasure1},
+    {'ingredient': drink.strIngredient2, 'measure': drink.strMeasure2},
+    {'ingredient': drink.strIngredient3, 'measure': drink.strMeasure3},
+    {'ingredient': drink.strIngredient4, 'measure': drink.strMeasure4},
+    {'ingredient': drink.strIngredient5, 'measure': drink.strMeasure5},
+    {'ingredient': drink.strIngredient6, 'measure': drink.strMeasure6},
+    {'ingredient': drink.strIngredient7, 'measure': drink.strMeasure7},
+    {'ingredient': drink.strIngredient8, 'measure': drink.strMeasure8},
+    {'ingredient': drink.strIngredient9, 'measure': drink.strMeasure9},
+    {'ingredient': drink.strIngredient10, 'measure': drink.strMeasure10},
+    {'ingredient': drink.strIngredient11, 'measure': drink.strMeasure11},
+    {'ingredient': drink.strIngredient12, 'measure': drink.strMeasure12},
+    {'ingredient': drink.strIngredient13, 'measure': drink.strMeasure13},
+    {'ingredient': drink.strIngredient14, 'measure': drink.strMeasure14},
+    {'ingredient': drink.strIngredient15, 'measure': drink.strMeasure15}
+  ];
+
+  let ingredients = [];
+  for(let i = 0; i < info.length; i++) {
+    if(info[i].ingredient === null) {
+      break;
+    }
+
+    ingredients.push(<li key={`ingredient-${i+1}`}>{info[i].ingredient} - {info[i].measure}</li>);
+  }
+
+  return ingredients;
 }
 
 
-const DrinkPage = (props: Props) => {
-  const [drink, setDrink] = useState([] as Drink[]);
+const DrinkPage = () => {
+  const [drink, setDrink] = useState<Drink | undefined>(undefined);
   const params = useParams();
   console.log(params);
   useEffect(() => {
@@ -19,21 +50,25 @@ const DrinkPage = (props: Props) => {
         return res.json()
       })
       .then((data) => {
-        return setDrink(data.drinks);
+        return setDrink(data.drinks[0]);
       })
     },[params.id])
     
-    console.log(drink)
+  const ingredients = getIngredients(drink);
+
   return (
     <div className="bigCardsContainer">
       <div>
-        <img src={drink[0]?.strDrinkThumb} alt="" />
+        <img src={drink?.strDrinkThumb} alt="" />
       </div>
       <div className="bigCardInfo">
-        <h1>{drink[0]?.strDrink}</h1>
+        <h1>{drink?.strDrink}</h1>
         <p>
-          {drink[0]?.strDrink}
+          {drink?.strDrink}
         </p>
+        <ul>
+          {ingredients}
+        </ul>
       </div>
     </div>
   )
